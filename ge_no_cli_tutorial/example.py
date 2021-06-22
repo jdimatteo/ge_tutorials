0af57798b613fdebfc9e4865dc89c6e9855e3041
John DiMatteo <jdimatteo@verily.com>
2021-06-22 11:46:46 -0600

    Updated to use pandas data

--- ge_no_cli_tutorial/example.py
+++ ge_no_cli_tutorial/example.py
@@ -2,7 +2,7 @@ import os
 import logging
 
 from great_expectations.data_context import BaseDataContext 
-from great_expectations.data_context.types.base import DataContextConfig
+from great_expectations.data_context.types.base import DataContextConfig, DatasourceConfig
 
 logging.basicConfig()
 logging.getLogger('sqlalchemy.engine').setLevel(logging.INFO)
@@ -30,23 +30,14 @@ def configure_data_context():
         plugins_directory=None,
         config_variables_file_path=None,
         datasources={
-            "my_postgres_db": {
-                "data_asset_type": {
-                    "class_name": "SqlAlchemyDataset",
+            "my_pandas": DatasourceConfig(
+                class_name="PandasDatasource",
+                module_name="great_expectations.datasource",
+                data_asset_type={
+                    "class_name": "PandasDataset",
                     "module_name": "great_expectations.dataset",
                 },
-                "class_name": "SqlAlchemyDatasource",
-                "module_name": "great_expectations.datasource",
-                "batch_kwargs_generators": {},
-                "credentials": {
-                    "drivername": "postgresql",
-                    "host": "localhost",
-                    "port": "65432",
-                    "username": "ge_tutorials",
-                    "password": "ge_tutorials",
-                    "database": "ge_tutorials"
-                }
-            },
+            )
         },
         stores={
             "expectations_store": {
@@ -116,9 +107,8 @@ def create_expectations(context):
         "my_suite_name", overwrite_existing=True
     )
     batch_kwargs = {
-        "datasource": "my_postgres_db",
-        "schema": "public",
-        "table": "yellow_tripdata_sample_2019_01",
+        "datasource": "my_pandas",
+        "path": "../data/yellow_tripdata_sample_2019-01.csv"
     }
     batch = context.get_batch(batch_kwargs, suite)
 
